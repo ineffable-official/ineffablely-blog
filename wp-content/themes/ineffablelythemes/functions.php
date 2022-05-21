22454d0217e11ee9ce9eae048b5d967e7a40b8e4
Faizun Musthofa <faizunmu00@gmail.com>
2022-05-21 10:28:38 +0700

    Full Change

--- wp-content/themes/ineffablelythemes/functions.php
+++ wp-content/themes/ineffablelythemes/functions.php
@@ -15,6 +15,7 @@ function add_theme_scripts()
     wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0', 'all');
 
     wp_enqueue_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array(), '5.1.3', true);
+    wp_enqueue_script('fontawesome', get_stylesheet_directory_uri() . '/assets/js/fontawesome.js', array(), '6.1.1', true);
 }
 
 add_action('wp_enqueue_scripts', 'add_theme_scripts');
